4a0fcf88ae42bd55811d2001967b2a6f3e2a6ae4
kvanama84 <46960075+kvanama84@users.noreply.github.com>
2019-06-12 16:08:37 +0530

    commit ing hear

---
+++ src/test/java/com/genericmethods/method_.java
@@ -0,0 +1,29 @@
+package com.genericmethods;
+
+public class method_ {
+	static int  a=5,b=6,c;
+	
+
+	public static void main(String[] args) {
+		Method1();
+	}
+		public   static void  Method1(){
+			
+		c=a+b;
+		System.out.println(c);
+		method2(c);
+
+	}
+		public static void method2(int k){
+			System.out.println(k+10);
+		}
+		public void palandrome(){
+			int in=654789,lastnumber,no=0;
+			while(in>0){
+				lastnumber=in%10;
+				
+			}
+			
+		}
+
+}
